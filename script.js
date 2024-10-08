7aa46e8eec3addbb752dea7e0c9a9f17803f0f24
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-10-08 21:02:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const balls = () => {let a = []; for(let x = 1; x < 50; x++) {a.push(x);} return a;}
+const sixBalls = () => {
+	const b = 6; let a = [];
+	for (let x = 1; x <= b; x++) {
+		const r = Math.floor(Math.random() * (balls().length) + 1);
+		if (r > 0 && a.length <= b) {
+			if (r != a.find(d => d === r)) {
+				a.push(r);
+			}
+			else {x--;}
+		}
+		else {x--;}
+	}
+	return a;
+}
+console.log(sixBalls());
