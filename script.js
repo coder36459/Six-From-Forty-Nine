a0651796794d81a384ebdca4852a780a187b1607
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-10-13 22:36:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const b4 = document.getElementById("b4");
 const b5 = document.getElementById("b5");
 const b6 = document.getElementById("b6");
 const playBtn = document.getElementById("play-btn");
+const stopBtn = document.getElementById("stop-btn");
 const resetBtn = document.getElementById("reset-btn");
 const result = document.getElementById("result");
 const balls = () => {let a = []; for(let x = 1; x < 50; x++) {a.push(x);} return a;}
@@ -22,62 +23,86 @@ const sixBalls = () => {
 	}
 	return a;
 }
-/*
-let c = false;
-const delay = p => {
-	let i = 0, a = [];
+
+const clear = c => {
+	c.innerText = "";
+	}
+
+let btn;
+	
+const delay = b => {
+	let i = 0, a = [], t;
 	const d = () => {
 		i++;
 		a.push(i);
-		if (p === 1 && c == 1) {
-			if (a.length > 1) {
-				ball1.innerText = "";
-				a.shift();
-			}
-			if (a[0] === 50) {
-				i = 1;
-			}
-			ball1.innerText = i;
+		if (a.length > 1) {
+			clear(b);
+			a.shift();
 		}
+		if (a[0] === 5) {
+			i = 1;
+		}
+		return b.innerText = i;
 	}
-	const t = setInterval(d, 10);
-	if (p === 1) {
-		return t;
+	if (!t) {
+		t = setInterval(d, 1000);
 	}
-	else if (p === 0) {
-		return clearInterval(t);
-	}
+	stopBtn.addEventListener("click", () => {
+		const f = sixBalls();
+		b1.innerText = f[0];
+		b2.innerText = f[1];
+		b3.innerText = f[2];
+		b4.innerText = f[3];
+		b5.innerText = f[4];
+		b6.innerText = f[5];
+		result.style.display = "block";
+		clearInterval(t);
+		t = null;
+		return btn = "stop";
+	});
 }
+
+
+
+//delay();
+/*
 const play = () => {
 	return c = true, delay(1);
 }
 setTimeout(play, 100);
 const score = () => {
-	return ball1.innerText = "", c = false, delay(0), ball1.innerText = c;
+	return console.clear(), c = false, delay(0), console.log(c);
 }
 setTimeout(score, 5000);
 */
-let b = false;
+
+
+
+
 playBtn.addEventListener("click", () => {
-	const a = sixBalls();
-	if (b === false) {
-		b1.innerText = a[0];
-		b2.innerText = a[1];
-		b3.innerText = a[2];
-		b4.innerText = a[3];
-		b5.innerText = a[4];
-		b6.innerText = a[5];
+	if (btn !== "play") {
 		result.style.display = "block";
+		b1.innerText = delay(b1);//a[0];
+		b2.innerText = delay(b2);//a[1];
+		b3.innerText = delay(b3);//a[2];
+		b4.innerText = delay(b4);//a[3];
+		b5.innerText = delay(b5);//a[4];
+		b6.innerText = delay(b6);//a[5];	
 	}
-	return b = true;
+	return btn = "play";
 });
-resetBtn.addEventListener("click", () => {
+
+const reset = () => {
 	b1.innerText = "";
 	b2.innerText = "";
 	b3.innerText = "";
 	b4.innerText = "";
 	b5.innerText = "";
 	b6.innerText = "";
+}
+
+resetBtn.addEventListener("click", () => {
+	reset();
 	result.style.display = "none";
-	return b = false;
+	return btn = "reset";
 });
