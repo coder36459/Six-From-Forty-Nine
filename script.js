4111e19fc45610a6d58fb46d6996d12b8643b750
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-10-15 20:16:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const ball = [document.getElementById("b1"), document.getElementById("b2"), docu
 const playBtn = document.getElementById("play-btn");
 const stopBtn = document.getElementById("stop-btn");
 const resetBtn = document.getElementById("reset-btn");
+const coupon = document.getElementById("coupon");
 const reset = () => {for (let x = 0; x < 6; x++) {ball[x].innerText = "$";}}
 const clear = c => {c.innerText = "";}
 const balls = () => {let a = []; for (let x = 1; x < 50; x++) {a.push(x);} return a;}
@@ -60,3 +61,26 @@ playBtn.addEventListener("click", () => {
 	playBtn.style.display = "none";
 	stopBtn.style.display = "inline-block";
 });
+const createCoupon = () => {
+	let a = [];
+	for (let x = 1; x <= 50; x++) {
+		if (x === 1 || x === 11 || x === 21 || x === 31 || x === 41) {
+			a.push("<tr><td>" + x + "</td>");
+		}
+		else if (x === 10 || x === 20 || x === 30 || x === 40 || x === 50) {
+			if (x === 50) {
+				a.push("<td></td></tr>");
+			}
+			else {
+				a.push("<td>" + x + "</td></tr>");
+			}
+			
+		}
+		else {
+			a.push("<td>" + x + "</td>");
+		}
+		
+	}
+	return coupon.innerHTML = `<table id="coupon-tab"><th colspan="10">Coupon</th>${a.join("")}</table>`
+}
+createCoupon();
